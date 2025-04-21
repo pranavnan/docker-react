5c3a9cdfa3c5121a0fa4d14deb954d196136c11f
pranavnan <pranavnan612@gmail.com>
2025-04-21 21:11:27 +0530

    first commit1

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,19 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  // Render the App component
   render(<App />);
+
+  // Check if the "Learn React" link is present in the document
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+test('renders learn react link1', () => {
+  // Render the App component
+  render(<App />);
+
+  // Check if the "Learn React" link is present in the document
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+  expect(0).toBe(1);
 });
